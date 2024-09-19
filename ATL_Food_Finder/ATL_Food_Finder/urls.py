edbbd4e5452694c59eaae40a3c8ee01274b4998d
Emily Hammond <Emilyhammonddd@gmail.com>
2024-09-19 08:44:51 -0400

    Add root URL redirection to search page

--- ATL_Food_Finder/ATL_Food_Finder/urls.py
+++ ATL_Food_Finder/ATL_Food_Finder/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.contrib import admin
+from django.urls import include, path
+
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('polls.urls')),  # Add this to direct the root URL to the polls app
 ]
--- ATL_Food_Finder/polls/urls.py
+++ ATL_Food_Finder/polls/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('search/', views.search_restaurants, name='search_restaurants'),
 ]
