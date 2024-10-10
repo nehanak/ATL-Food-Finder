b390b1f082acaddb5fce373b7040b5ffb0d7823e
Emily Hammond <Emilyhammonddd@gmail.com>
2024-10-10 10:13:55 -0400

    Changes to pop up when removing favorites, added reviews button

--- ATL_Food_Finder/ATL_Food_Finder/settings.py
+++ ATL_Food_Finder/ATL_Food_Finder/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ATL_Food_Finder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,3 +128,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/home/'
+LOGOUT_REDIRECT_URL = '/login/'
